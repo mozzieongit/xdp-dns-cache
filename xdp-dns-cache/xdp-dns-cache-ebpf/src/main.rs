0bb710c484674853cceac059e5e3807b115ecf9d
Jannik Peters <jannik.peters@os3.nl>
2024-01-11 14:43:12 +0100

    feat(xdp): return UDP packets from 127.0.0.2 back to sender

--- xdp-dns-cache/xdp-dns-cache-ebpf/src/main.rs
+++ xdp-dns-cache/xdp-dns-cache-ebpf/src/main.rs
@@ -28,26 +28,57 @@ fn try_xdp_dns_cache(ctx: XdpContext) -> Result<u32, ()> {
         _ => return Ok(xdp_action::XDP_PASS),
     }
 
-    let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+    let ipv4hdr: *mut Ipv4Hdr = ptr_at_mut(&ctx, EthHdr::LEN)?;
     let source_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+    let dest_addr = u32::from_be(unsafe { (*ipv4hdr).dst_addr });
 
-    let source_port = match unsafe { (*ipv4hdr).proto } {
+    let (source_port, dest_port) = match unsafe { (*ipv4hdr).proto } {
         IpProto::Tcp => {
-            let tcphdr: *const TcpHdr =
-                ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
-            u16::from_be(unsafe { (*tcphdr).source })
+            let tcphdr: *const TcpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            (
+                u16::from_be(unsafe { (*tcphdr).source }),
+                u16::from_be(unsafe { (*tcphdr).dest }),
+            )
         }
         IpProto::Udp => {
-            let udphdr: *const UdpHdr =
-                ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
-            u16::from_be(unsafe { (*udphdr).source })
+            let udphdr: *const UdpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            (
+                u16::from_be(unsafe { (*udphdr).source }),
+                u16::from_be(unsafe { (*udphdr).dest }),
+            )
         }
         _ => return Err(()),
     };
 
-    info!(&ctx, "SRC IP: {:i}, SRC PORT: {}", source_addr, source_port);
-
-    Ok(xdp_action::XDP_PASS)
+    info!(
+        &ctx,
+        "{:i}:{} => {:i}:{}", source_addr, source_port, dest_addr, dest_port
+    );
+    
+    // WARN: make sure to also change MAC addresses in future, the loopback
+    // devices uses a MAC of 0x00, so there is no issue when testing against lo
+    match source_addr {
+        // source == 127.0.0.2
+        0x7f000002 => {
+            info!(&ctx, "Changing and returning the packet");
+            unsafe {
+                (*ipv4hdr).dst_addr = u32::to_be(source_addr);
+                (*ipv4hdr).src_addr = u32::to_be(dest_addr);
+            };
+            match unsafe { (*ipv4hdr).proto } {
+                IpProto::Udp => {
+                    let udphdr: *mut UdpHdr = ptr_at_mut(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+                    unsafe {
+                        (*udphdr).source = u16::to_be(dest_port);
+                        (*udphdr).dest = u16::to_be(source_port);
+                    }
+                }
+                _ => return Err(()),
+            };
+            Ok(xdp_action::XDP_TX)
+        }
+        _ => Ok(xdp_action::XDP_PASS),
+    }
 }
 
 #[panic_handler]
@@ -65,4 +96,16 @@ fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
     }
 
     Ok((start + offset) as *const T)
+}
+
+fn ptr_at_mut<T>(ctx: &XdpContext, offset: usize) -> Result<*mut T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *mut T)
 }
