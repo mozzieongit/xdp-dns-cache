ac7731faf9a11d8d671589060fb927efdf871e04
Jannik Peters <jannik.peters@os3.nl>
2024-02-07 18:13:42 +0100

    chore(xdp): add note about metadata length check info source

--- xdp-dns-cache/xdp-dns-cache-ebpf/src/metadata.rs
+++ xdp-dns-cache/xdp-dns-cache-ebpf/src/metadata.rs
@@ -1,4 +1,5 @@
 // This struct needs to be a multiple of 4 bytes in size and at max 32 bytes in size
+// Check in kernel code in file include/net/xdp.h:xdp_metalen_invalid
 #[repr(C)]
 pub struct MetaData {
     pub dname_offset: u8,
